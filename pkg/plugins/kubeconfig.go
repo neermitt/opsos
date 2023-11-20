73bf6cc007b19b97daf6b89f521bdba28d1a87aa
neermitt <mittal.neeraj@gmail.com>
2023-11-20 08:54:03 +0530

    file kube config provider

--- pkg/plugins/kubeconfig.go
+++ pkg/plugins/kubeconfig.go
@@ -1,14 +1,19 @@
 package plugins
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
-	"log"
-
+	"github.com/go-playground/validator"
 	v1 "github.com/neermitt/opsos/api/v1"
 	"github.com/neermitt/opsos/pkg/config"
 	"github.com/neermitt/opsos/pkg/stack"
+	"github.com/neermitt/opsos/pkg/utils"
+	"github.com/spf13/afero"
+	"io"
+	"log"
+	"os"
 )
 
 type KubeConfigProvider interface {
@@ -56,4 +61,50 @@ func GetKubeConfig(ctx context.Context, k8sSettings map[string]any, stk *stack.S
 	log.Printf("[INFO] Writing the kubeconfig to file: %[1]s", kubeConfigPath)
 
 	return kubeConfigProvider.ExportKubeConfig(ctx, stk, providerSettings, kubeConfigPath)
+}
+
+type kubeFileProviderSettings struct {
+	Path string `yaml:"path" json:"path" mapstructure:"path" validate:"required"`
+}
+
+type kubeConfigFileProvider struct {
+}
+
+func (k kubeConfigFileProvider) ExportKubeConfig(_ context.Context, _ *stack.Stack, providerStackSettings map[string]any, kubeConfigPath string) error {
+	var settings kubeFileProviderSettings
+	err := utils.FromMap(providerStackSettings, &settings)
+
+	validate := validator.New()
+	if err = validate.Struct(&settings); err != nil {
+		return err
+	}
+
+	data, err := afero.ReadFile(afero.NewOsFs(), settings.Path)
+	if err != nil {
+		return err
+	}
+
+	return exportKindKubeConfigRaw(data, kubeConfigPath)
+}
+
+func exportKindKubeConfigRaw(kubeConfig []byte, kubeConfigPath string) (err error) {
+	f, err := os.OpenFile(kubeConfigPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		closeErr := f.Close()
+		if closeErr != nil && err == nil {
+			err = closeErr
+		}
+	}()
+
+	_, err = io.Copy(f, bytes.NewReader(kubeConfig))
+	return err
+}
+
+func init() {
+	RegisterKubeConfigProvider("file", func(conf *v1.ConfigSpec) (KubeConfigProvider, error) {
+		return kubeConfigFileProvider{}, nil
+	})
 }
